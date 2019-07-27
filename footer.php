5353bcd4d94ca307422d76e69decb3797c66df45
Arrow.Down <wangzhiqiang951753@gmail.com>
2019-07-27 21:32:54 +0800

    修改样式

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<footer id="footer" class="inner">
-			&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> 由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动 Theme is <a href="https://uefeng.com" target="_blank">Junichi</a>
+			&copy; 2018 - <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> 
 		</footer>
 	</div>
 </div>
--- post.php
+++ post.php
@@ -16,9 +16,9 @@
 					<p class="post-info">
 						最后更新于&nbsp;<span class="date"><?php echo date('Y-m-d  H:i:s' , $this->modified); ?></span>&nbsp;并被添加「<?php $this->tags(' ', true, ''); ?>」标签，已有 <?php get_post_view($this) ?> 位童鞋阅读过。
 					</p>
-					<p class="copyright-info">
+					<!-- <p class="copyright-info">
 						本站使用「<a target="_blank" href="http://creativecommons.org/licenses/by/4.0/deed.zh">署名 4.0 国际</a>」创作共享协议，可自由转载、引用，但需署名作者且注明文章出处</a>
-					</p>
+					</p> -->
 					<p>
 						<?php $this->related(5)->to($relatedPosts); ?>
 						<?php if (count($relatedPosts->stack)): ?>
