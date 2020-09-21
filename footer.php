9f9ece33c8ee7e93926b2b78fc0282946aa2063d
Arrow Down <wangzhiqiang951753@gmail.com>
2020-09-21 10:09:21 +0800

    remove cnzz

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 		<footer id="footer" class="inner">
 			&copy; 2018 - <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
-			<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278211958'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1278211958%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 		</footer>
 	</div>
 </div>
